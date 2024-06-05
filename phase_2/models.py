8a3ee49c670fb01701bf21abbf236edd145ee792
Sarel Lieberman <107625247+sarellieberman@users.noreply.github.com>
2024-06-05 14:29:31 +0300

    a

---
+++ phase_2/models.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+
+class Iris2LayerClassifier(nn.Module):
+    def __init__(self):
+        super(Iris2LayerClassifier, self).__init__()
+        self.classifier = nn.Sequential(
+            nn.Linear(4, 5),
+            nn.ReLU(),
+            nn.Linear(5, 3)
+        )
+
+    def forward(self, x):
+        x = self.linear(x)
+        return x
+    
