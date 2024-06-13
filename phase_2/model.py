6b5d1c21e3b8823ed99396ba532189147e619cee
shilo-a7x <107671225+shilo-a7x@users.noreply.github.com>
2024-06-13 10:35:47 +0300

    bad model

--- phase_2/model.py
+++ phase_2/model.py
@@ -1,15 +1,11 @@
 import torch.nn as nn
 
+
 class Iris2LayerClassifier(nn.Module):
     def __init__(self):
         super(Iris2LayerClassifier, self).__init__()
-        self.classifier = nn.Sequential(
-            nn.Linear(4, 5),
-            nn.ReLU(),
-            nn.Linear(5, 3)
-        )
+        self.classifier = nn.Sequential(nn.Linear(4, 5), nn.ReLU(), nn.Linear(5, 3))
 
     def forward(self, x):
         x = self.classifier(x)
         return x
-    
