fa83d48e70172faff16ea43ade1bed8b9174ceb5
shilo-a7x <107671225+shilo-a7x@users.noreply.github.com>
2024-10-10 01:40:55 +0300

    minor changes

--- phase_3_spiral/data.py
+++ phase_3_spiral/data.py
@@ -1,11 +1,5 @@
-# Code for creating a spiral dataset from CS231n
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-# # lets visualize the data
-# plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.RdYlBu)
-# plt.show()
 
 
 def create_spiral(N=100, K=3, gamma=0.5):
@@ -14,17 +8,12 @@ def create_spiral(N=100, K=3, gamma=0.5):
     # N = 100  # number of points per class
     D = 2  # dimensionality
     # K = 3  # number of classes
-    X = np.zeros((N * K, D))  # data matrix (each row = single example)
-    y = np.zeros(N * K, dtype="uint8")  # class labels
+    X = np.zeros((N * K, D))
+    y = np.zeros(N * K, dtype="uint8")
     for j in range(K):
         ix = range(N * j, N * (j + 1))
-        r = np.linspace(0.0, 1, N)  # radius
-        t = np.linspace(j * 4, (j + 1) * 4, N) + np.random.randn(N) * gamma  # theta
+        r = np.linspace(0.0, 1, N)
+        t = np.linspace(j * 4, (j + 1) * 4, N) + np.random.randn(N) * gamma
         X[ix] = np.c_[r * np.sin(t), r * np.cos(t)]
         y[ix] = j
     return X, y
-
-
-# X, y = create_spiral(gamma=0.1)
-# plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.RdYlBu)
-# plt.show()
